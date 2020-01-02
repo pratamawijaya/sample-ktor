b99df3d0e536293da59ceff2a5be10e725871dea
pratamawijaya <set.mnemonix@gmail.com>
2020-01-02 11:44:21 +0700

    4 - use routing

--- src/Application.kt
+++ src/Application.kt
@@ -2,25 +2,40 @@ package com.pratama
 
 import io.ktor.application.*
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty,
+    embeddedServer(
+        Netty,
         port = 3030,
         host = "localhost",
-        module = Application::mainModule)
-    .start(wait = true)
+        module = Application::mainModule
+    )
+        .start(wait = true)
 }
 
 fun Application.mainModule() {
     routing {
-        get("/") {
-            call.respondText("Hello World")
-        }
+        root()
+    }
+}
+
+fun Routing.root() {
+    get("/") {
+        call.respondText("Hello World")
+    }
+    get("/profile") {
+        call.respondText(
+            contentType = ContentType.Application.Json,
+            status = HttpStatusCode.OK,
+            text = "user"
+        )
     }
 }
